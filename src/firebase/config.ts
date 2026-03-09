d54628cf590a5c21aeb09ad283ba151216450575
bikram dey <bikram20031213dey@gmail.com>
2026-03-09 09:21:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1543002889-2a319",
+  "appId": "1:79774390975:web:219bba09e346a7adae440a",
+  "apiKey": "AIzaSyBAuYRmr8rBWZUEGdyybIfrVwIBe-guj4Y",
+  "authDomain": "studio-1543002889-2a319.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "79774390975"
+};
